82883691c01eeb5de2d51319a2bb616f7bebbd42
mayala3 <mayala3@stanford.edu>
2019-02-16 17:37:58 -0800

    added entity extractor

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -1,4 +1,6 @@
 from urllib.parse import urlparse
+import spacy
+import re
 
 def is_same_url(url1,url2):
     parse1 = urlparse(url1)
@@ -11,3 +13,21 @@ def is_same_url(url1,url2):
         return False
 
     return True
+
+def get_entities(text):
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(text)
+
+    ent_counts = {}
+
+    for ent in doc.ents:
+        if ent.label_ in ["ORDINAL","CARDINAL","DATE","TIME","QUANTITY","PERCENT"]:
+            continue
+        formatted = re.sub('\n',' ',ent.text)
+        formatted = re.sub('[^a-zA-Z0-9 ]','',formatted)
+        if formatted not in ent_counts:
+            ent_counts[formatted] = 1
+        else:
+            ent_counts[formatted] += 1
+
+    return sorted(ent_counts.keys(),key=lambda x: ent_counts[x],reverse=True)[:3]
